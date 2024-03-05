862587ae9cac03dbe0b243154524611514ff3683
Moti Leggese <motileggese@gmail.com>
2024-03-05 16:51:59 +0300

    LAB-1-Classes-and-Objects-exercise-3

--- LAB-1-Classes-and-Objects.dart
+++ LAB-1-Classes-and-Objects.dart
@@ -11,10 +11,23 @@ class Car {
   }
 }
 
+//exercise 3
+class ElectricCar extends Car {
+  int batteryLife;
+
+  ElectricCar(String brand, String model, int year, this.batteryLife)
+      : super(brand, model, year);
+}
+
 void main() {
   //exercise 1
   Car myCar = Car('Toyota', 'Corolla', 2020);
   print('Brand: ${myCar.brand}, Model: ${myCar.model}, Year: ${myCar.year}');
   //exercise 2
   myCar.makeNoise();
+  //exercise 3
+  ElectricCar myElectricCar = ElectricCar('Tesla', 'Model S', 2020, 500);
+  print(
+      'Brand: ${myElectricCar.brand}, Model: ${myElectricCar.model}, Year: ${myElectricCar.year}, Battery Life: ${myElectricCar.batteryLife} hours');
+  myElectricCar.makeNoise();
 }
