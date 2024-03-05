7e0030866d33f002d3dcb0bd55d3078d85afe781
Moti Leggese <motileggese@gmail.com>
2024-03-05 16:37:18 +0300

    LAB-1-Collections (List, Set, Map)-exercise-2

--- LAB-1-Collections (List, Set, Map).dart
+++ LAB-1-Collections (List, Set, Map).dart
@@ -3,7 +3,21 @@ int findHighest(List<int> numbers) {
   return numbers.reduce((curr, next) => curr > next ? curr : next);
 }
 
+//exercise 2
+void printMap(Map map) {
+  map.forEach((key, value) {
+    print('Key: $key, Value: $value');
+  });
+}
+
 void main() {
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   print('The highest number is ${findHighest(numbers)}');
+
+  Map<String, int> map = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+  };
+  printMap(map);
 }
