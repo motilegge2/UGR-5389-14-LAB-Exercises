9e71852ca1df330c7ae039c8ce073125462bc0c7
Moti Leggese <motileggese@gmail.com>
2024-03-05 16:48:33 +0300

    LAB-1-Classes-and-Objects-exercise-2

--- LAB-1-Classes-and-Objects.dart
+++ LAB-1-Classes-and-Objects.dart
@@ -5,10 +5,16 @@ class Car {
   int year;
 
   Car(this.brand, this.model, this.year);
+  //exercise 2
+  void makeNoise() {
+    print('Vroom Vroom');
+  }
 }
 
 void main() {
   //exercise 1
   Car myCar = Car('Toyota', 'Corolla', 2020);
   print('Brand: ${myCar.brand}, Model: ${myCar.model}, Year: ${myCar.year}');
+  //exercise 2
+  myCar.makeNoise();
 }
